adb6b92c05f3a55a361376ea2af97fed73ec08e4
Gus <kosta.anastasakis@gmail.com>
2022-02-05 13:09:36 -0500

    Revert "Force test failure"
    This reverts commit 801260ee81039c36e97d3e23cf679f51d8b99a19.

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -11,7 +11,7 @@ def test_fib():
     """API Tests"""
     assert fib(1) == 1
     assert fib(2) == 1
-    assert fib(7) == 11
+    assert fib(7) == 13
     with pytest.raises(AssertionError):
         fib(-10)
 
